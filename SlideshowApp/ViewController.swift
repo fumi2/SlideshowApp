04119c40397a651a5032981bdaa2c5d3d2af0f78
Fumitaka Hijino <Hijino@hijino-no-macbook-puro-2017.local>
2018-01-27 21:42:33 +0900

    進むボタンと戻るボタンを実装

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 各画像を定義
+    var image1 = #imageLiteral(resourceName: "smallDog1.jpg")
+    var image2 = #imageLiteral(resourceName: "smallDog2.jpg")
+    var image3 = #imageLiteral(resourceName: "smallDog3.jpg")
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 最初の画像をセット
+        image.image = image1
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // 進むボタンの動作
+    @IBAction func forwardButton(_ sender: Any) {
+        
+        //1枚目の時
+        if image.image == image1 {
+        image.image = image2
+    }
+        //2枚目の時
+        else if image.image == image2 {
+            image.image = image3
+        }
+        
+        //3枚目の時
+        else if image.image == image3 {
+            image.image = image1
+        }
 }
 
+    // 戻るボタンの動作
+    @IBAction func returnButton(_ sender: Any) {
+        
+        //1枚目の時
+        if image.image == image1 {
+            image.image = image3
+        }
+            //2枚目の時
+        else if image.image == image2 {
+            image.image = image1
+        }
+            
+            //3枚目の時
+        else if image.image == image3 {
+            image.image = image2
+        }
+        
+    }
+    
+    
+}
